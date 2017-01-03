80db66184ca1463207606835d01a27d4e84da221
Philipp Wesner <pwesner@innomind.info>
2017-01-03 13:54:54 +0100

    Add require_relative for vis-only-timeline

--- vendor/assets/javascripts/vis-only-timeline.js
+++ vendor/assets/javascripts/vis-only-timeline.js
@@ -1,23 +1,23 @@
-//= require ../vis/module/header
+//= require_relative ../vis/module/header
 //= require moment
 //= require hammer
-//= require ../component/emitter
+//= require_relative ../component/emitter
 
-//= require ../vis/shim
-//= require ../vis/util
-//= require ../vis/DataSet
-//= require ../vis/DataView
+//= require_relative ../vis/shim
+//= require_relative ../vis/util
+//= require_relative ../vis/DataSet
+//= require_relative ../vis/DataView
 
-//= require ../vis/timeline/stack
-//= require ../vis/timeline/TimeStep
-//= require ../vis/timeline/component/Component
-//= require ../vis/timeline/Range
-//= require ../vis/timeline/component/TimeAxis
-//= require ../vis/timeline/component/CurrentTime
-//= require ../vis/timeline/component/CustomTime
+//= require_relative ../vis/timeline/stack
+//= require_relative ../vis/timeline/TimeStep
+//= require_relative ../vis/timeline/component/Component
+//= require_relative ../vis/timeline/Range
+//= require_relative ../vis/timeline/component/TimeAxis
+//= require_relative ../vis/timeline/component/CurrentTime
+//= require_relative ../vis/timeline/component/CustomTime
 //= require_tree ../vis/timeline/component/item
-//= require ../vis/timeline/component/ItemSet
-//= require ../vis/timeline/component/Group
-//= require ../vis/timeline/Timeline
+//= require_relative ../vis/timeline/component/ItemSet
+//= require_relative ../vis/timeline/component/Group
+//= require_relative ../vis/timeline/Timeline
 
 //= require ./module/exports-only-timeline
